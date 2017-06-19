1567ffc3889413520e9b90da84e14d9e9ce2d4d9
arieo <aacc1631@gmail.com>
2017-06-19 21:28:00 +0300

    Initial commit

---
+++ app/src/main/java/app2/example/com/app2/Action.java
@@ -0,0 +1,26 @@
+package app2.example.com.app2;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Action extends Fragment {
+    public static Action newInstance() {
+        Action fragment = new Action();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_action, container, false);
+    }
+}
---
+++ app/src/main/java/app2/example/com/app2/Business.java
@@ -0,0 +1,25 @@
+package app2.example.com.app2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Business extends Fragment {
+    public static Business newInstance() {
+        Business fragment = new Business();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_business, container, false);
+    }
+}
