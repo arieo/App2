989f4d2c8993fe5cc21eadc99e25a90c3949e010
arieo <aacc1631@gmail.com>
2017-06-20 20:23:26 +0300

    +Receiver

--- app/src/main/java/app2/example/com/app2/Action.java
+++ app/src/main/java/app2/example/com/app2/Action.java
@@ -1,13 +1,19 @@
 package app2.example.com.app2;
 
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class Action extends Fragment {
+
+    Cursor mCursor;
+
     public static Action newInstance() {
         Action fragment = new Action();
         return fragment;
@@ -15,7 +21,15 @@ public class Action extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        Uri mUri = Uri.parse("content://com.example.loginapplication.Model.BackEnd.BusinessAndActionProvider/business");
+        try{
+            mCursor = getActivity().getApplicationContext().getContentResolver().query(mUri, null, null, null, null);
+        }catch (Exception e) {
+            Log.d("EXCEPTION", e.toString());
+        }
+        int a = mCursor.getColumnCount();
     }
 
     @Override
--- app/src/main/java/app2/example/com/app2/Business.java
+++ app/src/main/java/app2/example/com/app2/Business.java
@@ -1,5 +1,7 @@
 package app2.example.com.app2;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,13 +9,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class Business extends Fragment {
+    Cursor mCursor;
     public static Business newInstance() {
         Business fragment = new Business();
         return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
     }
 
